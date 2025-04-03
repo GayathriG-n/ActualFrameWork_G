c421ad4f80e96dc9bd92bdb3e7bed6b9a8568695
Dell <Dell@DESKTOP-9KRK1A9>
2025-04-03 11:32:33 +0530

    test script written for create contact and create org

--- Project_G/src/test/java/contactTest/CreateContactTest.java
+++ Project_G/src/test/java/contactTest/CreateContactTest.java
@@ -4,6 +4,8 @@ public class CreateContactTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Test script wriiten for create contact");
+		System.out.println("Contact created successfully");
 
 	}
 
--- Project_G/src/test/java/organizationTest/ToCreateOrganization.java
+++ Project_G/src/test/java/organizationTest/ToCreateOrganization.java
@@ -4,6 +4,8 @@ public class ToCreateOrganization {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Test script wriiten for create org");
+		System.out.println("Org created successfully");
 
 	}
 
